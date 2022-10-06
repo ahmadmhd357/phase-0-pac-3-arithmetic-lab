1525c457becb6157d8a270661e18168f159528b6
Ahmad Hariri <ahmadmhd357@gmail.com>
2022-10-06 05:10:33 +0300

    completed

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(x,y){
+return x + y ;
+}
+add(5,3);
+function subtract(x,y){
+    return x - y ;
+    }
+subtract(5,3);
+function multiply(x,y){
+    return x * y ;
+    }
+multiply(5,3);
+function divide(x,y){
+    return x / y ;
+    }
+divide(5,3);
+function increment(n){
+    return ++n ;
+    }
+increment(5);
+function decrement(n){
+    return --n ;
+    }
+decrement(5);
+function makeInt(n){
+    return parseInt(n,10);
+}
+makeInt("16")
+
+
+function preserveDecimal(n){
+   return parseFloat(n);
+}
+preserveDecimal("5.26")
